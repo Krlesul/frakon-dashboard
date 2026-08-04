6dc801cf8dee7aa2d8265d6ffe8010c98411616e
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-04 23:41:22 +0200

    add Vite build

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: () => 'frakon-dashboard.js',
+    },
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+  test: {
+    environment: 'node',
+  },
+});
