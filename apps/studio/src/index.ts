d2a1c0d507552e40d7255dd08fa4694433253a63
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-06 15:34:51 +0200

    export Studio history components

--- apps/studio/src/index.ts
+++ apps/studio/src/index.ts
@@ -5,4 +5,6 @@ export * from './constraint-inspector';
 export * from './constraint-preview-overlay';
 export * from './constraint-preview-bridge';
 export * from './dashboard-studio';
+export * from './dashboard-studio-history';
+export * from './history-toolbar';
 export * from './guideline-overlay';
