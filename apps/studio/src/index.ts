392b233b105425163ba35c1ec880028a400f8a95
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-06 13:59:02 +0200

    export Studio guideline overlay

--- apps/studio/src/index.ts
+++ apps/studio/src/index.ts
@@ -2,3 +2,4 @@ export * from './studio-canvas';
 export * from './surface-style-editor';
 export * from './surface-inspector';
 export * from './dashboard-studio';
+export * from './guideline-overlay';
