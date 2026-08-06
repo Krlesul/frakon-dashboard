6d9245948cfe8d418a49655d2ce2dfc81d0a1a72
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-06 14:45:09 +0200

    export constraint preview overlay

--- apps/studio/src/index.ts
+++ apps/studio/src/index.ts
@@ -2,5 +2,6 @@ export * from './studio-canvas';
 export * from './surface-style-editor';
 export * from './surface-inspector';
 export * from './constraint-inspector';
+export * from './constraint-preview-overlay';
 export * from './dashboard-studio';
 export * from './guideline-overlay';
