a961ba3f182cd8dc12e310cd02155925a706fee4
Krlesul <132875575+Krlesul@users.noreply.github.com>
2026-08-07 20:48:54 +0200

    add dashboard document version policy constants

--- custom_components/frakon_dashboard/const.py
+++ custom_components/frakon_dashboard/const.py
@@ -3,3 +3,6 @@ STORAGE_KEY = "frakon_dashboard.dashboards"
 STORAGE_VERSION = 1
 DATA_BACKEND = "backend"
 DATA_WEBSOCKET_REGISTERED = "websocket_registered"
+
+READABLE_DOCUMENT_VERSIONS = frozenset({1, 2})
+WRITABLE_DOCUMENT_VERSIONS = frozenset({1})
